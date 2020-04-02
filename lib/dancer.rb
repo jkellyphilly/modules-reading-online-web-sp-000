2cf04cf112a22be1d8f79ea059557ec559480c78
Joel Kelly <joel.kelly.philly@gmail.com>
2020-04-02 19:29:48 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  include Dance
+  include Dance # includes the dance module
 
   attr_accessor :name
 
