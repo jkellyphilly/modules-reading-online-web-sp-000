45bc386b8bdaeca74e9b733383d380974d4d51a2
Joel Kelly <joel.kelly.philly@gmail.com>
2020-04-02 19:29:58 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,12 +2,12 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  include Dance
+  include Dance # includes the dance module
 
   attr_accessor :name
 
   def initialize(name)
     @name = name
   end
-  
+
 end
